39f5c131117d62a2c525f9b1a8db1e84dae2ae3c
领琾 <825238111@qq.com>
2019-06-27 00:15:21 +0800

    1

---
+++ SwiftUILearning/SwiftUILearning/SwiftUIView.swift
@@ -0,0 +1,23 @@
+//
+//  SwiftUIView.swift
+//  SwiftUILearning
+//
+//  Created by 领琾 on 2019/6/23.
+//  Copyright © 2019 txooo. All rights reserved.
+//
+
+import SwiftUI
+
+struct SwiftUIView : View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+struct SwiftUIView_Previews : PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
+#endif
